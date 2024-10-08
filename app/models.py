f263211202571755acbe2cafddd890c14333165c
abachleda-baca <agnieszka.bachleda@digital.justice.gov.uk>
2024-10-08 14:05:40 +0100

    added flask db migration tool to app

---
+++ app/models.py
@@ -0,0 +1,13 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Acronym(db.Model):
+        __tablename__ = 'acronyms'
+        id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+        abbreviation = db.Column(db.String, nullable=False)
+        definition = db.Column(db.String, nullable=False)
+        url = db.Column(db.String, nullable=True)
+        description = db.Column(db.String, nullable=True)
+
+        __table_args__ = (db.UniqueConstraint('abbreviation', 'definition', name='_abbreviation_definition_uc'),)
