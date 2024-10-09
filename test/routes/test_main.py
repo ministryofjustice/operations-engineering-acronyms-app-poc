c24319fe138d0a4f183c9bba5d78f49a5cd244d9
abachleda-baca <agnieszka.bachleda@digital.justice.gov.uk>
2024-10-09 11:54:31 +0100

    updated tc

--- test/routes/test_main.py
+++ test/routes/test_main.py
@@ -1,12 +1,23 @@
 import unittest
+from unittest.mock import patch
+from app.app import create_app
 
 
 class MainRouteTestCase(unittest.TestCase):
-    def setUp(self):
-        pass
+
+    @patch('app.models.db.init_app')
+    @patch('flask_migrate.Migrate')
+    def setUp(self, mock_migrate, mock_init_app):
+        self.mock_migrate = mock_migrate  # Store the mock_migrate instance
+        self.mock_init_app = mock_init_app
+        self.app = create_app(False)
+        self.app.config["SECRET_KEY"] = "test_flask"
+        self.app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://test_user:test_password@localhost:5432/test_db"
+        self.client = self.app.test_client()
 
     def test_index(self):
-        pass
+        response = self.client.get("/")
+        self.assertEqual(response.status_code, 200)
 
 
 if __name__ == "__main__":
