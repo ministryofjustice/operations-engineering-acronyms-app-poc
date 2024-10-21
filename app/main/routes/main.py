48413f190d88124b5fa0b6d96c5c1421be2b92ba
abachleda-baca <agnieszka.bachleda@digital.justice.gov.uk>
2024-10-21 16:03:59 +0100

    modified main route to display data from db

--- app/main/routes/main.py
+++ app/main/routes/main.py
@@ -1,8 +1,13 @@
 from flask import Blueprint, current_app, redirect, render_template, request
+from ...models import Acronym
+
 
 main = Blueprint("main", __name__)
 
-
 @main.route("/")
 def index():
-    return render_template("pages/main.html")
+
+    # Query users from the database (example)
+    acronyms = Acronym.query.all()
+
+    return render_template('pages/main.html', acronyms=acronyms)
