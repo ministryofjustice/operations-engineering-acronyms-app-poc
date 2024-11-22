dccf4e3b46f5ba648d81e9261c04afeeb001ab27
abachleda-baca <agnieszka.bachleda@digital.justice.gov.uk>
2024-11-22 16:49:36 +0000

    enabled search functionality

--- app/main/routes/main.py
+++ app/main/routes/main.py
@@ -1,14 +1,24 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
 from ...models import Acronym
-
 
 main = Blueprint("main", __name__)
 
 
-@main.route("/")
+@main.route("/", methods=["GET", "POST"])
 def index():
+    search_term = ""  
 
-    # Query users from the database (example)
-    acronyms = Acronym.query.all()
+    
+    if request.method == "POST":
+        search_term = request.form.get("name", "").strip()
+        
+        return redirect(url_for('main.index', name=search_term))  
 
-    return render_template('pages/main.html', acronyms=acronyms)
+    search_term = request.args.get('name', '') 
+    if search_term:
+        acronyms = Acronym.query.filter(Acronym.abbreviation.ilike(f"%{search_term}%")).all()
+
+    else:
+        acronyms = Acronym.query.all()
+
+    return render_template("pages/main.html", acronyms=acronyms, search_term=search_term)
